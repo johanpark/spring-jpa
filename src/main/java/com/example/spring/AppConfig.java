8644eec8cbdf4070159ecd6afd910ad91a263550
johanpark <pyhan23@gmail.com>
2021-12-12 20:18:29 +0900

    2021-12-12 commit

--- src/main/java/com/example/spring/AppConfig.java
+++ src/main/java/com/example/spring/AppConfig.java
@@ -9,21 +9,28 @@ import com.example.spring.member.MemberServiceImpl;
 import com.example.spring.member.MemoryMemberRepository;
 import com.example.spring.order.OrderService;
 import com.example.spring.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
--- src/main/java/com/example/spring/OrderApp.java
+++ src/main/java/com/example/spring/OrderApp.java
@@ -7,13 +7,21 @@ import com.example.spring.member.MemberServiceImpl;
 import com.example.spring.order.Order;
 import com.example.spring.order.OrderService;
 import com.example.spring.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
+/*        AppConfig appConfig = new AppConfig();
         MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        OrderService orderService = appConfig.orderService();*/
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
